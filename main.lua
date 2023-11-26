ba0a359a1eaffe6c53de0c9f260f56dbde5efa44
shjshshsjsu <122176104+shjshshsjsu@users.noreply.github.com>
2023-11-26 23:49:01 +0300

    Update main.lua

--- main.lua
+++ main.lua
@@ -1,13 +1,12 @@
-local lib = loadstring(game:HttpGet('https://raw.githubusercontent.com/loglizzy/lib/main/main.lua'))()
+local lib = loadstring(game:HttpGet("https://raw.githubusercontent.com/shjshshsjsu/My-Lib/main/Source.lua", true))()
 local window = lib:Window('title here')
 
 window:Toggle('toggle', function(enabled)
      print(enabled)
 end)
 
-window:Slider('slider', {
-     max = 80,
-     def = 20
-}, function(value)
-     print(value)
+window:Button('Button', function(enabled)
+     print(Button)
 end)
+
+
